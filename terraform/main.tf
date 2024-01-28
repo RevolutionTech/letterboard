93ec5aab51a9220566cc46103f99e650588ef38e
Lucas Connors <lucas.revolutiontech@gmail.com>
2024-01-28 00:04:30 -0800

    build: bump simple-static-website to e6c2d3efb1915f30d2bccb3c2b4c57804a062875

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,7 +14,7 @@ provider "aws" {
 }
 
 module "simple-static-website" {
-  source = "github.com/RevolutionTech/opstrich//terraform/modules/simple-static-website"
+  source = "git@github.com:RevolutionTech/opstrich.git//terraform/modules/simple-static-website?ref=e6c2d3efb1915f30d2bccb3c2b4c57804a062875"
 
   github_repo = "letterboard"
   domain_name = "letterboard.revolutiontech.ca"
